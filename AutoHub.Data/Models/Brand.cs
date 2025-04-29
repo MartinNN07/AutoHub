51711084d00cc18f064e804d1954f0a62aa1bc13
Martin <martinnedev07@gmail.com>
2025-04-29 08:55:09 +0300

    Created Brand.cs file

--- AutoHub.Data/Models/Brand.cs
+++ AutoHub.Data/Models/Brand.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +11,16 @@ namespace AutoHub.Data.Models
 {
 	internal class Brand
 	{
-		int Id { get; set; }
-		string Name { get; set; }
-		string CountryOfOrigin { get; set; }
-		ICollection<Car> Cars { get; set; }
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string Name { get; set; }
+
+		[MaxLength(50)]
+		public string? CountryOfOrigin { get; set; }
+
+		public ICollection<Car>? Cars { get; set; }
 	}
 }
