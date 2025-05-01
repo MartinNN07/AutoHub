5a655104472e5f9288861ba6da68629dc4d61b70
Александър Михайлов <alexmihaylov07@gmail.com>
2025-05-01 22:39:30 +0300

    Added Service

---
+++ AutoHub.Business/Services/IBrandService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoHub.Business.Services
+{
+    internal interface IBrandService
+    {
+        Task<Brand> CreateBrandAsync(Brand brand);
+
+        // READ
+        Task<IEnumerable<Brand>> GetAllBrandsAsync();
+        Task<Brand> GetBrandByIdAsync(int id);
+        Task<IEnumerable<Brand>> GetBrandsByNameAsync(string searchTerm);
+
+        // UPDATE
+        Task<Brand> UpdateBrandAsync(Brand brand);
+
+        // DELETE
+        Task<bool> DeleteBrandAsync(int id);
+    }
+}
