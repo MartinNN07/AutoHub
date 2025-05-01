9de2fbfe63423da064fa282b03b5c10a9a08a007
MartinNN07 <martinnedev07@gmail.com>
2025-05-01 21:06:51 +0300

    Created AutoHubDbContext

---
+++ AutoHub.Data/Database/AutoHubDbContext.cs
@@ -0,0 +1,62 @@
+﻿using AutoHub.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoHub.Data.Database
+{
+	internal class AutoHubDbContext :DbContext
+	{
+		public DbSet<Car> Cars { get; set; }
+		public DbSet<Brand> Brands { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Salesperson> Salespersons { get; set; }
+		public DbSet<Sale> Sales { get; set; }
+
+		public AutoHubDbContext(DbContextOptions<AutoHubDbContext> options)
+			: base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+
+			modelBuilder.Entity<Car>()
+				.HasOne(c => c.Brand)
+				.WithMany(b => b.Cars)
+				.HasForeignKey(c => c.BrandId)
+				.OnDelete(DeleteBehavior.Restrict); 
+
+			modelBuilder.Entity<Sale>()
+				.HasOne(s => s.Car)
+				.WithOne()
+				.HasForeignKey<Sale>(s => s.CarId)
+				.OnDelete(DeleteBehavior.Restrict); 
+
+			modelBuilder.Entity<Sale>()
+				.HasOne(s => s.Customer)
+				.WithMany(c => c.Sales)
+				.HasForeignKey(s => s.CustomerId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			modelBuilder.Entity<Sale>()
+				.HasOne(s => s.Salesperson)
+				.WithMany(sp => sp.Sales)
+				.HasForeignKey(s => s.SalespersonId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			modelBuilder.Entity<Salesperson>()
+				.HasIndex(s => s.EmployeeNumber)
+				.IsUnique();
+
+			modelBuilder.Entity<Sale>()
+				.HasIndex(s => s.CarId)
+				.IsUnique();
+		}
+	}
+}
--- AutoHub.Data/Models/Sale.cs
+++ AutoHub.Data/Models/Sale.cs
@@ -1,41 +1,42 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AutoHub.Data.Models
 {
+	[Index(nameof(CarId), IsUnique = true)]
 	internal class Sale
 	{
-        [Key]
-        public int Id { get; set; }
+		[Key]
+		public int Id { get; set; }
 
-        [Required]
-        public DateTime SaleDate { get; set; }
+		[Required]
+		public DateTime SaleDate { get; set; }
 
-        [Required]
-        [Range(0.01, 15000000)]
-        public decimal SalePrice { get; set; }
+		[Required]
+		[Range(0.01, 15000000)]
+		public decimal SalePrice { get; set; }
 
-        [Required]
-        [Index(IsUnique = true)]
-        public int CarId { get; set; }
+		[Required]
+		public int CarId { get; set; }
 
-        [Required]
-        public virtual Car Car { get; set; }
+		[Required]
+		public virtual Car Car { get; set; }
 
-        [Required]
-        public int CustomerId { get; set; }
+		[Required]
+		public int CustomerId { get; set; }
 
-        [Required]
-        public virtual Customer Customer { get; set; }
+		[Required]
+		public virtual Customer Customer { get; set; }
 
-        [Required]
-        public int SalespersonId { get; set; }
+		[Required]
+		public int SalespersonId { get; set; }
 
-        [Required]
-        public virtual Salesperson Salesperson { get; set; }
-
-    }
+		[Required]
+		public virtual Salesperson Salesperson { get; set; }
+	}
 }
