655c2d96fbdc53605d9ef29bbe5c8ebd08054371
MartinNN07 <martinnedev07@gmail.com>
2025-05-01 20:16:11 +0300

    Added elements Customer.cs

--- AutoHub.Data/Models/Customer.cs
+++ AutoHub.Data/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,26 @@ namespace AutoHub.Data.Models
 {
 	internal class Customer
 	{
+		[Key]
+		public int Id { get; set; }
 
+		[Required]
+		[MaxLength(50)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string LastName { get; set; }
+
+		[MaxLength(100)]
+		[EmailAddress] // Email validation attribute
+		public string Email { get; set; }
+
+		[Required]
+		[MaxLength(20)]
+		public string PhoneNumber { get; set; }
+
+		// Navigation property for sales related to this customer
+		public virtual ICollection<Sale> Sales { get; set; }
 	}
 }
--- AutoHub.Data/Models/Sale.cs
+++ AutoHub.Data/Models/Sale.cs
@@ -8,5 +8,6 @@ namespace AutoHub.Data.Models
 {
 	internal class Sale
 	{
+
 	}
 }
