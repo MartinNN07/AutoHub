7f58f162223064aa48f4609a2f184ed74d1ab23e
TheCrock1 <propeepesen@abv.bg>
2025-05-02 11:41:07 +0300

    Created Views and Controllers

---
+++ AutoHub/Controllers/BrandController.cs
@@ -0,0 +1,14 @@
+﻿using AutoHub.Business.Services;
+using AutoHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoHub.Business.Controllers
+{
+    public class BrandController
+    {
+    }
+}
